1e3a1eb45266de28509613abd6820f90341a4a5a
Josh Urbain <josh.urbain@covenanteyes.com>
2024-09-24 16:05:23 -0400

    feat(): Added platform designation to full_build.sh and added example line for a Docker network

--- full_build.sh
+++ full_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker rm -f -v "vclod${1:+-$1}"
-docker build --tag vclods:1.0 .
+docker build --platform linux/amd64 --tag vclods:1.0 .
 docker container run -i -t -d -v $(realpath ./scripts):/app/scripts -v $(realpath ./oracle_config):/app/oracle_config --name "vclod${1:+-$1}" vclods:1.0
+# Example: for using a specific network, use this line instead of the above one
+# docker container run -i -t -d -v $(realpath ./scripts):/app/scripts -v $(realpath ./oracle_config):/app/oracle_config --network=mariadb-network --name "vclod${1:+-$1}" vclods:1.0
 docker exec -it "vclod${1:+-$1}" /bin/bash
